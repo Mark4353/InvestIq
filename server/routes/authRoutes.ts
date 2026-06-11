45ce2206190e5509f3a5227fbce6a4f748a97c98
der1x <130235333+Mark4353@users.noreply.github.com>
2026-06-11 13:51:32 +0300

    Updates

--- server/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { login, register, me } from '../controllers/authController.ts'
-import { requireAuth } from '../middleware/authMiddleware.ts'
-
-export const authRouter = Router()
-
-authRouter.post('/register', register)
-authRouter.post('/login', login)
-authRouter.get('/me', requireAuth, me)
--- server/routes/transactionsRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { listAll, create, remove } from '../controllers/transactionsController'
-import { requireAuth } from '../middleware/authMiddleware'
-
-export const transactionsRouter = Router()
-
-transactionsRouter.get('/', requireAuth, listAll)
-transactionsRouter.post('/', requireAuth, create)
-transactionsRouter.delete('/:id', requireAuth, remove)
